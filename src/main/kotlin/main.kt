4b50b39295c45bba730e887cbc7b4261a2c770c8
lalit17 <lalitrajput069@gmail.com>
2020-12-23 16:58:44 +0530

    Just added COMMENT

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,16 @@
-fun main() {
-    var a = 2 // var can be modified
-    val constant =3 // val can't be modified, its like final variable in java
 
-    println("Hello World!$a$constant")
+fun main(args:Array<String>) {
+    // we can assign function RETURN TYPE to VARIABLE directly through FUNCTION
+    var a:Int = lalit(2,3);
 
+    // val can't be modified, its like final variable in java
+    var constant =3
+    // $ sign is use to differentiate VARIABLE from STRING
+    println("Hello World!$a")
+
+}
+
+// this is how FUNCTION work in KOTLIN
+fun lalit(value:Int, value2:Int) : Int  {
+    return(25)
 }
